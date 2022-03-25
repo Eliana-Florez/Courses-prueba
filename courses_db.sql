5adaf2a194a70bad08708ff16b1b92152771287d
Eliana-Florez <evflorezc@itc.edu.co>
2022-03-25 10:09:01 -0500

    base de datos

---
+++ courses_db.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-03-2022 a las 16:08:14
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `courses_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nameCourse` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hours` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `updated_at` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `courses`
+--
+
+INSERT INTO `courses` (`id`, `nameCourse`, `hours`, `updated_at`, `created_at`) VALUES
+(1, 'Ingles', '20', '2022-03-25 08:52:10', '2022-03-25 08:52:10'),
+(2, 'Java', '100', '2022-03-25 08:52:26', '2022-03-25 08:52:26'),
+(4, 'MySQL', '80', '2022-03-25 09:01:14', '2022-03-25 09:01:14'),
+(5, 'HTML', '20', '2022-03-25 09:50:06', '2022-03-25 09:50:06'),
+(6, 'Python', '80', '2022-03-25 09:50:49', '2022-03-25 09:50:49');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_courses_table', 1),
+(2, '2014_10_12_000000_create_users_table', 1),
+(3, '2014_10_12_100000_create_password_resets_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `confirmPass` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `rol` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `courses` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `lastName`, `email`, `phone`, `password`, `confirmPass`, `rol`, `courses`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', '-', 'admin@mail.com', '3225817845', '$2y$10$/Qo.Eo5UUnG2b2LcJajXeeyqb7nVOQ6oIqEH52Usk50yJElmxdwRu', '123456789', 'Administrador', NULL, NULL, NULL, '2022-03-25 14:46:41', '2022-03-25 14:46:56'),
+(2, 'Luisa', 'Cardozo', 'luisa@mail.com', '355847154', '$2y$10$7qNPHRmk5UZ03ADoH7U1hexPXtBAnWnzcAb/jZuvPaJvczNQ00ttS', '123456789', 'Alumno/a', '[\"Java\",\"MySQL\"]', NULL, NULL, '2022-03-25 14:47:39', '2022-03-25 14:51:36'),
+(3, 'Sebastian', 'Cespedes', 'sebastian@mail.com', '3669854512', '$2y$10$8X3BXWIsDe6O9Rn4NyYES.c.6m.Sg/w0HtHPyo.F.OU/imr7/mSi2', '123456789', 'Alumno/a', '[\"Ingles\",\"Python\"]', NULL, NULL, '2022-03-25 14:48:20', '2022-03-25 14:51:43'),
+(4, 'Paula', 'Maldonado', 'paula@mail.com', '322514784', '$2y$10$dNnba1.8mBKxczI/xqvgeujJN/TkDJ2/kvnMJOhFRjeiXobngeyDS', '123456789', 'Alumno/a', '[\"Java\",\"MySQL\",\"HTML\"]', NULL, NULL, '2022-03-25 14:48:56', '2022-03-25 14:51:51'),
+(5, 'Arbey', 'Perdomo', 'arbey@mail.com', '3225147845', '$2y$10$ordrxeKG65oCJsfM696tBe/xm3u.e7N5bUOEdSbgvf3GA.aK0lKo.', '123456789', 'Alumno/a', '[\"Ingles\",\"Java\",\"MySQL\",\"HTML\"]', NULL, NULL, '2022-03-25 14:49:41', '2022-03-25 14:52:01');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
